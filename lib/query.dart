4a3ace342d553a91e2f26624a81a12ea28b4104b
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-11-05 19:25:15 +0530

    Integration of backend done

--- lib/query.dart
+++ lib/query.dart
@@ -1,7 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class QueryPage extends StatefulWidget {
   QueryPage({Key? key}) : super(key: key);
@@ -13,8 +14,9 @@ class QueryPage extends StatefulWidget {
 class _QueryPageState extends State<QueryPage> {
   SpeechToText _speechToText = SpeechToText();
   bool _speechEnabled = false;
-  String _words = '';
+  String _query = '';
   TextEditingController _controller = TextEditingController();
+  List<ChatMessage> _messages = [];
 
   @override
   void initState() {
@@ -40,8 +42,33 @@ class _QueryPageState extends State<QueryPage> {
   void _onSpeechResult(SpeechRecognitionResult result) {
     _controller.text = result.recognizedWords;
     setState(() {
-      _words = result.recognizedWords;
+      _query = result.recognizedWords;
     });
+  }
+
+  Future<void> _sendMessage(String message) async {
+    final query = message;
+    const serverUrl = 'https://geobot-backend.onrender.com/api/categorize'; // Replace with your server's URL
+
+    final response = await http.post(
+      Uri.parse(serverUrl),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({'text': query}),
+    );
+
+    if (response.statusCode == 200) {
+      final serverResponse = response.body;
+      // Add the user's message and the server's response to the chat
+      setState(() {
+        _messages.add(ChatMessage(text: _query, isUserMessage: true));
+        _messages.add(ChatMessage(text: serverResponse, isUserMessage: false));
+      });
+    } else {
+      // Handle server errors or other cases
+      print('Failed to send the query to the server. Status code: ${response.statusCode}');
+    }
   }
 
   @override
@@ -50,41 +77,72 @@ class _QueryPageState extends State<QueryPage> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Geobot Qwery!"),
+          title: const Text("Geobot Query!"),
         ),
-        body: Align(
-          alignment: Alignment.bottomCenter,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end, // Align content at the bottom
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    suffix: IconButton(
-                      onPressed:
-                          _speechToText.isListening ? _stopListening : _startListening,
-                      icon: Icon(
-                        _speechToText.isListening ? Icons.mic_off : Icons.mic,
-                      ),
-                      iconSize: 30,
-                      color: const Color.fromRGBO(226, 152, 5, 1),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  final message = _messages[index];
+                  return ListTile(
+                    title: Align(
+                      alignment: message.isUserMessage ? Alignment.centerRight : Alignment.centerLeft,
+                      child: Text(message.text),
                     ),
-                    hintText: 'Enter your Query here',
-                  ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                padding: EdgeInsets.only(bottom: 10),  // Add padding to the bottom
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: 'Enter your Query here',
+                          prefixIcon: IconButton(
+                            onPressed: _speechToText.isListening ? _stopListening : _startListening,
+                            icon: Icon(_speechToText.isListening ? Icons.mic_off : Icons.mic),
+                            color: const Color.fromRGBO(226, 152, 5, 1),
+                          ),
+                        ),
+                      ),
+                    ),
+                    ClipOval(  // Wrap the IconButton with ClipOval to make it round
+                      child: Material(
+                        color: const Color.fromRGBO(226, 152, 5, 1),
+                        child: IconButton(
+                          onPressed: () {
+                            final userMessage = _controller.text;
+                            _controller.clear();
+                            _sendMessage(userMessage);
+                          },
+                          icon: Icon(Icons.send),
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              // Text(
-              //       _speechToText.isListening
-              //         ? _words
-              //         : '',
-              //     ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+class ChatMessage {
+  final String text;
+  final bool isUserMessage;
+
+  ChatMessage({required this.text, required this.isUserMessage});
 }
--- lib/querysample.dart
+++ lib/querysample.dart
@@ -1,7 +1,147 @@
+// import 'package:flutter/material.dart';
+// import 'package:speech_to_text/speech_recognition_result.dart';
+// import 'package:speech_to_text/speech_to_text.dart';
+// import 'package:http/http.dart' as http;
+
+// class QueryPage extends StatefulWidget {
+//   QueryPage({Key? key}) : super(key: key);
+
+//   @override
+//   _QueryPageState createState() => _QueryPageState();
+// }
+
+// class _QueryPageState extends State<QueryPage> {
+//   SpeechToText _speechToText = SpeechToText();
+//   bool _speechEnabled = false;
+//   String _query = '';
+//   TextEditingController _controller = TextEditingController();
+//   List<ChatMessage> _messages = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initSpeech();
+//   }
+
+//   void _initSpeech() async {
+//     _speechEnabled = await _speechToText.initialize();
+//     setState(() {});
+//   }
+
+//   void _stopListening() async {
+//     await _speechToText.stop();
+//     setState(() {});
+//   }
+
+//   void _startListening() async {
+//     await _speechToText.listen(onResult: _onSpeechResult);
+//     setState(() {});
+//   }
+
+//   void _onSpeechResult(SpeechRecognitionResult result) {
+//     _controller.text = result.recognizedWords;
+//     setState(() {
+//       _query = result.recognizedWords;
+//     });
+//   }
+
+//   Future<void> _sendMessage(String message) async {
+//     final query = message;
+//     final serverUrl = 'https://geobot-backend.onrender.com/api/categorize'; // Replace with your server's URL
+
+//     final response = await http.post(
+//       Uri.parse(serverUrl),
+//       headers: {'Content-Type': 'application/json',},
+//       body: {'text': query},
+      
+//     );
+
+//     if (response.statusCode == 200) {
+//       final serverResponse = response.body;
+//       // Add the user's message and the server's response to the chat
+//       setState(() {
+//         _messages.add(ChatMessage(text: _query, isUserMessage: true));
+//         _messages.add(ChatMessage(text: serverResponse, isUserMessage: false));
+//       });
+//     } else {
+//       // Handle server errors or other cases
+//       print('Failed to send the query to the server. Status code: ${response.statusCode}');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Geobot Query!"),
+//         ),
+//         body: Column(
+//           children: [
+//             Expanded(
+//               child: ListView.builder(
+//                 itemCount: _messages.length,
+//                 itemBuilder: (context, index) {
+//                   final message = _messages[index];
+//                  return ListTile(
+//                    title: Align(
+//                     alignment: message.isUserMessage ? Alignment.centerRight : Alignment.centerLeft,
+//                     child: Text(message.text),
+//                   ),
+//                 );
+
+//                 },
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.all(16),
+//               child: TextField(
+//                 controller: _controller,
+//                 decoration: InputDecoration(
+//                   border: const OutlineInputBorder(),
+//                   hintText: 'Enter your Query here',
+//                 ),
+//               ),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: [
+//                 IconButton(
+//                   onPressed: _speechToText.isListening ? _stopListening : _startListening,
+//                   icon: Icon(_speechToText.isListening ? Icons.mic_off : Icons.mic),
+//                   iconSize: 30,
+//                   color: const Color.fromRGBO(226, 152, 5, 1),
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     final userMessage = _controller.text;
+//                     _controller.clear();
+//                     _sendMessage(userMessage);
+//                   },
+//                   child: Text('Send'),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class ChatMessage {
+//   final String text;
+//   final bool isUserMessage;
+
+//   ChatMessage({required this.text, required this.isUserMessage});
+// }
+
+
+
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-import 'package:http/http.dart' as http;
 
 class QueryPage extends StatefulWidget {
   QueryPage({Key? key}) : super(key: key);
@@ -13,9 +153,8 @@ class QueryPage extends StatefulWidget {
 class _QueryPageState extends State<QueryPage> {
   SpeechToText _speechToText = SpeechToText();
   bool _speechEnabled = false;
-  String _query = '';
+  String _words = '';
   TextEditingController _controller = TextEditingController();
-  List<ChatMessage> _messages = [];
 
   @override
   void initState() {
@@ -41,30 +180,8 @@ class _QueryPageState extends State<QueryPage> {
   void _onSpeechResult(SpeechRecognitionResult result) {
     _controller.text = result.recognizedWords;
     setState(() {
-      _query = result.recognizedWords;
+      _words = result.recognizedWords;
     });
-  }
-
-  Future<void> _sendMessage(String message) async {
-    final query = message;
-    final serverUrl = 'YOUR_SERVER_URL'; // Replace with your server's URL
-
-    final response = await http.post(
-      Uri.parse(serverUrl),
-      body: {'query': query},
-    );
-
-    if (response.statusCode == 200) {
-      final serverResponse = response.body;
-      // Add the user's message and the server's response to the chat
-      setState(() {
-        _messages.add(ChatMessage(text: _query, isUserMessage: true));
-        _messages.add(ChatMessage(text: serverResponse, isUserMessage: false));
-      });
-    } else {
-      // Handle server errors or other cases
-      print('Failed to send the query to the server. Status code: ${response.statusCode}');
-    }
   }
 
   @override
@@ -73,64 +190,41 @@ class _QueryPageState extends State<QueryPage> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Geobot Query!"),
+          title: const Text("Geobot Qwery!"),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: _messages.length,
-                itemBuilder: (context, index) {
-                  final message = _messages[index];
-                 return ListTile(
-                   title: Align(
-                    alignment: message.isUserMessage ? Alignment.centerRight : Alignment.centerLeft,
-                    child: Text(message.text),
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end, // Align content at the bottom
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    suffix: IconButton(
+                      onPressed:
+                          _speechToText.isListening ? _stopListening : _startListening,
+                      icon: Icon(
+                        _speechToText.isListening ? Icons.mic_off : Icons.mic,
+                      ),
+                      iconSize: 30,
+                      color: const Color.fromRGBO(226, 152, 5, 1),
+                    ),
+                    hintText: 'Enter your Query here',
                   ),
-                );
-
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: 'Enter your Query here',
                 ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  onPressed: _speechToText.isListening ? _stopListening : _startListening,
-                  icon: Icon(_speechToText.isListening ? Icons.mic_off : Icons.mic),
-                  iconSize: 30,
-                  color: const Color.fromRGBO(226, 152, 5, 1),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    final userMessage = _controller.text;
-                    _controller.clear();
-                    _sendMessage(userMessage);
-                  },
-                  child: Text('Send'),
-                ),
-              ],
-            ),
-          ],
+              // Text(
+              //       _speechToText.isListening
+              //         ? _words
+              //         : '',
+              //     ),
+            ],
+          ),
         ),
       ),
     );
   }
-}
-
-class ChatMessage {
-  final String text;
-  final bool isUserMessage;
-
-  ChatMessage({required this.text, required this.isUserMessage});
 }
