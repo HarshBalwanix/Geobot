5f194f76a7907988158ba6de000a1dd4c5d2d709
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-12-17 01:10:34 +0530

    Added apk

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,6 @@ class ChatApp extends StatefulWidget {
   State<ChatApp> createState() => _ChatAppState();
 }
 
-
-
 void _launchURL(int value) async {
   String url = "";
   if (value == 1) {
@@ -34,11 +32,13 @@ void _launchURL(int value) async {
         "https://play.google.com/store/apps/details?id=com.mitf.SaharaMobile&hl=en_IN&gl=US&pli=1";
   }
   if (await canLaunch(url)) {
-    await launch(url, forceSafariVC: true, forceWebView: true, enableJavaScript: true);
+    await launch(url,
+        forceSafariVC: true, forceWebView: true, enableJavaScript: true);
   } else {
     throw "Could not launch $url";
   }
 }
+
 class _ChatAppState extends State<ChatApp> {
   final List<String> imageList = [
     "https://pbs.twimg.com/profile_images/1171839413879365632/NaEa4KKY_400x400.jpg",
@@ -47,47 +47,50 @@ class _ChatAppState extends State<ChatApp> {
     "https://pbs.twimg.com/profile_banners/1533051649681305603/1654343880/1500x500",
   ];
 
-  Widget _BScard(){
+  Widget _BScard() {
     return Card(
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-            child: Image.asset("assets/isromain.jpeg",
-              fit: BoxFit.fill,
-              width: 150.0,
-              height: 150.0,
-            ),
-          );
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Image.asset(
+        "assets/isromain.jpeg",
+        fit: BoxFit.fill,
+        width: 150.0,
+        height: 150.0,
+      ),
+    );
   }
 
-  Widget _BSDetails(){
+  Widget _BSDetails() {
     return const SizedBox(
-            width: double.infinity,
-            height: 100.0, // Adjust the height to decrease the distance
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.only(left: 20, right: 20, top: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Text("ISRO", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                    ),
-                    SizedBox(
-                      width: 135,
-                      child: Divider(),
-                    ),
-                    Center(
-                      child: Text("Proud of India ❤️"),
-                    ),
-                    SizedBox(
-                      width: 170,
-                      child: Divider(),
-                    ),
-                  ],
-                ),
+      width: double.infinity,
+      height: 100.0, // Adjust the height to decrease the distance
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text("ISRO",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
               ),
-            ),
-          );
+              SizedBox(
+                width: 135,
+                child: Divider(),
+              ),
+              Center(
+                child: Text("Proud of India ❤️"),
+              ),
+              SizedBox(
+                width: 170,
+                child: Divider(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
@@ -97,61 +100,78 @@ class _ChatAppState extends State<ChatApp> {
         title: const Text("Geobot"),
         centerTitle: true,
       ),
-      drawer:  Drawer(
-       child:  ListView(
-        children: [
-                 const UserAccountsDrawerHeader(
-                   accountName: Text("Hi SAHARA!!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),),
-                   accountEmail: null,
-                   currentAccountPicture: null,
-           ),
-             const Divider(
-                color:  Colors.orange, // Change the color to your preferred color
-                thickness: 2, // Adjust the thickness as needed
-                // indent: 20, // Adjust the starting point from the left
-                // endIndent: 20, // Adjust the ending point from the right
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text(
+                "Hi SAHARA!!",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
               ),
+              accountEmail: null,
+              currentAccountPicture: null,
+            ),
+            const Divider(
+              color: Colors.orange, // Change the color to your preferred color
+              thickness: 2, // Adjust the thickness as needed
+              // indent: 20, // Adjust the starting point from the left
+              // endIndent: 20, // Adjust the ending point from the right
+            ),
             ListTile(
-            title:  const Text("ISRO Site!",style: TextStyle( fontSize: 17),),
-            leading: Image.asset('assets/logo.png', width: 35, height: 35), // Adjust width and height as needed
-            onTap: (){
-              _launchURL(1);
-            },
-           ),
-           const Divider(
-                color: Colors.lightBlue, // Change the color to your preferred color
-                thickness: 2, // Adjust the thickness as needed
-                indent: 20, // Adjust the starting point from the left
-                endIndent: 20, // Adjust the ending point from the right
+              title: const Text(
+                "ISRO Site!",
+                style: TextStyle(fontSize: 17),
               ),
-           ListTile(
-            title: const Text("Locate on map",style: TextStyle( fontSize: 17),),
-            leading:const Icon(Icons.maps_home_work_rounded),
-            onTap: (){
-              _launchURL(2);
-            },
-           ),
-           const Divider(
-                color: Colors.lightBlue, // Change the color to your preferred color
-                thickness: 2, // Adjust the thickness as needed
-                indent: 20, // Adjust the starting point from the left
-                endIndent: 20, // Adjust the ending point from the right
+              leading: Image.asset('assets/logo.png',
+                  width: 35, height: 35), // Adjust width and height as needed
+              onTap: () {
+                _launchURL(1);
+              },
+            ),
+            const Divider(
+              color:
+                  Colors.lightBlue, // Change the color to your preferred color
+              thickness: 2, // Adjust the thickness as needed
+              indent: 20, // Adjust the starting point from the left
+              endIndent: 20, // Adjust the ending point from the right
+            ),
+            ListTile(
+              title: const Text(
+                "Locate on map",
+                style: TextStyle(fontSize: 17),
               ),
-           ListTile(
-            title: const Text("About SAHARA",style: TextStyle( fontSize: 17),),
-            leading: const Icon(Icons.question_mark),
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>saharapage()));
-            },
-           ),
-           const Divider(
-                color: Colors.lightBlue, // Change the color to your preferred color
-                thickness: 2, // Adjust the thickness as needed
-                indent: 20, // Adjust the starting point from the left
-                endIndent: 20, // Adjust the ending point from the right
+              leading: const Icon(Icons.maps_home_work_rounded),
+              onTap: () {
+                _launchURL(2);
+              },
+            ),
+            const Divider(
+              color:
+                  Colors.lightBlue, // Change the color to your preferred color
+              thickness: 2, // Adjust the thickness as needed
+              indent: 20, // Adjust the starting point from the left
+              endIndent: 20, // Adjust the ending point from the right
+            ),
+            ListTile(
+              title: const Text(
+                "About SAHARA",
+                style: TextStyle(fontSize: 17),
               ),
-        ],
-       ), 
+              leading: const Icon(Icons.question_mark),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => saharapage()));
+              },
+            ),
+            const Divider(
+              color:
+                  Colors.lightBlue, // Change the color to your preferred color
+              thickness: 2, // Adjust the thickness as needed
+              indent: 20, // Adjust the starting point from the left
+              endIndent: 20, // Adjust the ending point from the right
+            ),
+          ],
+        ),
       ),
       body: ListView(
         padding: const EdgeInsets.only(top: 40.0),
@@ -163,37 +183,39 @@ class _ChatAppState extends State<ChatApp> {
                 enableInfiniteScroll: true,
                 autoPlay: true,
               ),
-              items: imageList.map((e) => ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Image.network(
-                      e,
-                      width: 1050.0,
-                      height: 350.0,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              )).toList(),
+              items: imageList
+                  .map((e) => ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Image.network(
+                              e,
+                              width: 1050.0,
+                              height: 350.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ],
+                        ),
+                      ))
+                  .toList(),
             ),
           ),
-         const Divider(height: 50.0),
-         _BScard(),
-         _BSDetails(),
+          const Divider(height: 50.0),
+          _BScard(),
+          _BSDetails(),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-  onPressed: () {
-    Navigator.push(context, MaterialPageRoute(builder: (context)=>QueryPage()));
-  },
-  icon: const Icon(Icons.question_answer),
-  label: const Text("Enter Query!"),
-  tooltip: 'Connect to Geobot',
-  backgroundColor: Colors.lightBlue,
-),
- 
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => QueryPage()));
+        },
+        icon: const Icon(Icons.question_answer),
+        label: const Text("Enter Query!"),
+        tooltip: 'Connect to Geobot',
+        backgroundColor: Colors.lightBlue,
+      ),
     );
   }
 }
